e64bee375f1b23ea2a51eb2f3dbfc387b484bd63
zahid-blueeye <zahidaxmedov.313gmail.com>
2024-02-11 00:54:49 +0400

    theme mui correction

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
