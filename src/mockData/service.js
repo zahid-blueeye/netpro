e856c47b2483f5cda0d25a84627713d291b70fe7
zahid-blueeye <zahidaxmedov.313gmail.com>
2024-02-11 21:12:46 +0400

    data service

---
+++ src/mockData/service.js
@@ -0,0 +1,33 @@
+const services = [
+  {
+    id: 0,
+    title: "Avadanlıqların quraşdırılması və konfiqurasiyası",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna diam ullamcorper aliquet non, ut id sit eros integer.",
+  },
+  {
+    id: 1,
+    title: "Daimi nəzarət",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna diam ullamcorper aliquet non, ut id sit eros integer.",
+  },
+  {
+    id: 2,
+    title: "Avadanlıqların vaxtında dəyişdirilməsi",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna diam ullamcorper aliquet non, ut id sit eros integer.",
+  },
+  {
+    id: 3,
+    title: "İT xidmətlər",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna diam ullamcorper aliquet non, ut id sit eros integer.",
+  },
+  {
+    id: 4,
+    title: "Helpdesk xidmətlər",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna diam ullamcorper aliquet non, ut id sit eros integer.",
+  },
+  {
+    id: 5,
+    title: "Təhlükəsizlik xidmətləri",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna diam ullamcorper aliquet non, ut id sit eros integer.",
+  },
+];
+export default services;
